bdf23fa7db7a7bcb86813d6956193c0024d09a25
Mnhttn92 <64465623+Mnhttn92@users.noreply.github.com>
2023-04-22 09:00:25 -0700

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("click", function(e){
+   const targetElement = e.target;
+   if (targetElement.closest('.header__burger')){
+      document.documentElement.classList.toggle('menu-open');
+      e.preventDefault();
+   }
+})
